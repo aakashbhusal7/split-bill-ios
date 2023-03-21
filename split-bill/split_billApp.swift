2059c96989376cc64de07a6bfe1b220d1c03657a
aakashbhusal <aakash.bhusal@8squarei.com>
2023-03-21 16:11:41 +0545

    Initial Commit

---
+++ split-bill/split_billApp.swift
@@ -0,0 +1,17 @@
+//
+//  split_billApp.swift
+//  split-bill
+//
+//  Created by Aakash Bhusal on 21/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct split_billApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
